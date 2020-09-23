b4028781235dbd8eb93523db8f7c316af6bffc74
D3m1urg0 <p.caruso86@gmail.com>
2020-09-23 12:47:25 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+import "./plugins/axios";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "./assets/tailwind.css";
 
 createApp(App)
   .use(store)
