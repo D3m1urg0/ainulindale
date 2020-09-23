60944324089099887d232cc4a5910170ed5f1897
D3m1urg0 <p.caruso86@gmail.com>
2020-09-23 16:42:47 +0200

    vee validate needs update

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import axios from "./plugins/axios";
+// import veeValidate from "./plugins/vee-validate";
 import "./assets/tailwind.css";
 
 createApp(App)
   .use(store)
   .use(router)
   .use(axios)
+  // .use(veeValidate)
   .mount("#app");
