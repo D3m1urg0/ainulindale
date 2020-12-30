689a4dd459082a6bf4db43909d991a35d6410588
Pietro.Caruso <p.caruso86@gmail.com>
2020-12-30 10:44:11 +0100

    working schema + vee-validate & yup

---
+++ src/components/form/fields/FormCheckbox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <label>
+      <input
+        type="checkbox"
+        :checked="modelValue"
+        @input="update($event.target.checked)"
+      />
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script>
+***REMOVED***
+  props: {
+    modelValue: { required: true },
+    required: {
+      type: Boolean,
+      default: false
+  ***REMOVED***,
+    config: {
+      type: Object,
+      default: () => {}
+  ***REMOVED***,
+    label: {
+      type: String,
+      required: true
+  ***REMOVED***
+***REMOVED***,
+  methods: {
+    update(value) {
+      this.$emit("update:modelValue", value);
+  ***REMOVED***
+***REMOVED***
+***REMOVED***
+</script>
