17777bbefa9b1e55c30fd509bb121ef03571155f
Pietro.Caruso <p.caruso86@gmail.com>
2021-01-04 10:45:53 +0100

    textarea

---
+++ src/components/form/fields/FormTextArea.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <label :for="uuid" v-html="labelHtml" />
+    <textarea
+      :value="modelValue"
+      :required="required"
+      :id="uuid"
+      :disabled="readOnly"
+      @input="update($event.target.value)"
+    />
+    <p v-show="validation.errorMessage" v-text="validation.errorMessage"></p>
+  </div>
+</template>
+
+<script>
+***REMOVED***
+  props: {
+    modelValue: { required: true },
+    required: {
+      type: Boolean,
+      default: false
+  ***REMOVED***,
+    label: {
+      type: String,
+      required: true
+  ***REMOVED***,
+    config: {
+      type: Object,
+      default: () => ({ type: "text" })
+  ***REMOVED***,
+    readOnly: {
+      type: Boolean,
+      default: false
+  ***REMOVED***,
+    uuid: {
+      type: Number,
+      default: 0
+  ***REMOVED***,
+    validation: {
+      type: Object,
+      default: () => ({})
+  ***REMOVED***
+***REMOVED***,
+  computed: {
+    labelHtml() {
+      return `${this.label} <span class="label--required">*</span>`;
+  ***REMOVED***
+***REMOVED***,
+  methods: {
+    update(value) {
+      this.$emit("update:modelValue", value);
+  ***REMOVED***
+***REMOVED***
+***REMOVED***
+</script>
