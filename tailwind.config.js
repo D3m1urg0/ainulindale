6458b34faca7bb527fc8f5cf4eaeed4c871c3a6d
Pietro.Caruso <p.caruso86@gmail.com>
2021-01-08 14:17:51 +0100

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {}
