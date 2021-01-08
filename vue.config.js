89b6d5fdc3c409dae47b13d46f25e2c22415c8be
Pietro.Caruso <p.caruso86@gmail.com>
2021-01-08 14:31:48 +0100

    auto routing working

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   pluginOptions: {
     moment: {
       locales: ["it"]
+  ***REMOVED***,
+    autoRouting: {
+      chunkNamePrefix: "page-"
   ***REMOVED***
 ***REMOVED***
 ***REMOVED***
