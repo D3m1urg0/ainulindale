16c48e19886d72460c8669863f2cf88e024b6c1b
Pietro Caruso <pietro.caruso@agictech.com>
2021-03-11 10:25:19 +0100

    localized validation

--- src/plugins/formvuelate.js
+++ src/plugins/formvuelate.js
@@ -3,25 +3,33 @@ import { SchemaWizard, SchemaFormFactory } from "formvuelate";
 import VeeValidatePlugin from "@formvuelate/plugin-vee-validate";
 import LookupPlugin from "@formvuelate/plugin-lookup";
 
-import { localize } from "@vee-validate/i18n";
+import { localize, setLocale } from "@vee-validate/i18n";
 import it from "@vee-validate/i18n/dist/locale/it.json";
+import { configure } from "vee-validate";
+import { defineRule } from "vee-validate";
+import * as AllRules from "@vee-validate/rules";
+
+Object.keys(AllRules).forEach(rule => {
+  defineRule(rule, AllRules[rule]);
+});
+
+configure({
+  generateMessage: localize({ it }),
+  classes: {
+    valid: "is-valid",
+    invalid: "is-invalid",
+    dirty: ["is-dirty", "is-dirty"] // multiple classes per flag!
+***REMOVED***
+});
+
+setLocale("it");
 
 const SchemaFormWithPlugins = SchemaFormFactory([
+  VeeValidatePlugin({}),
   LookupPlugin({
     mapComponents: {
       string: "FormText",
       array: "FormSelect"
-  ***REMOVED***
-***REMOVED***),
-  VeeValidatePlugin({
-    // plugin configuration here
-    generateMessage: localize({
-      it
-  ***REMOVED***),
-    classes: {
-      valid: "is-valid",
-      invalid: "is-invalid",
-      dirty: ["is-dirty", "is-dirty"] // multiple classes per flag!
   ***REMOVED***
 ***REMOVED***)
 ]);
