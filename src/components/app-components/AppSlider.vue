8bec5544946230c8e42245107f3502a2bc8ca477
Pietro Caruso <pietro.caruso@agictech.com>
2021-03-15 15:56:34 +0100

    swiper

---
+++ src/components/app-components/AppSlider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex">
+    <app-navigation-carets v-if="items">
+      <swiper v-bind="$attrs">
+        <swiper-slide v-for="(item, i) in [...items]" :key="i">
+          <slot name="slide" v-bind="item" />
+        </swiper-slide>
+      </swiper>
+    </app-navigation-carets>
+  </div>
+</template>
+
+<script>
+***REMOVED***
+  props: ["items"]
+***REMOVED***
+</script>
