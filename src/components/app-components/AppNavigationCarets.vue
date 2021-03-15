379ccd088fe803e2accfe083f8fb587149f74ff1
Pietro Caruso <pietro.caruso@agictech.com>
2021-03-15 15:56:34 +0100

    swiper

---
+++ src/components/app-components/AppNavigationCarets.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="relative flex max-w-full">
+    <svg
+      id="prev-caret"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 10 24"
+      stroke="currentColor"
+      class="flex-none h-10 my-auto cursor-pointer"
+      :class="{ 'md:hidden': mobileOnly }"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M8 19l-7-7 7-7"
+      />
+    </svg>
+    <slot />
+    <svg
+      id="next-caret"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 10 24"
+      stroke="currentColor"
+      class=" flex-none h-10 my-auto cursor-pointer"
+      :class="{ 'md:hidden': !allResolution }"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M1 5l7 7-7 7"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+***REMOVED***
+  props: {
+    mobileOnly: Boolean
+***REMOVED***
+***REMOVED***
+</script>
