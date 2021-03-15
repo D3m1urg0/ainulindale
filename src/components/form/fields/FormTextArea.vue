a525ff5fd08a18fda36a697b171d5077be99f2fc
Pietro Caruso <pietro.caruso@agictech.com>
2021-03-15 15:55:18 +0100

    lodash in form

--- src/components/form/fields/FormTextArea.vue
+++ src/components/form/fields/FormTextArea.vue
@@ -13,6 +13,7 @@
 </template>
 
 <script>
+import _ from "lodash";
 ***REMOVED***
   emits: ["update:modelValue"],
   props: {
@@ -49,7 +50,8 @@
   computed: {
     labelHtml() {
       const requiredHtml =
-        this.validations._exclusive && this.validations._exclusive.required
+        (this.validations._exclusive && this.validations._exclusive.required) ||
+        _.includes(this.validations, "required")
           ? ' <span class="label--required">*</span>'
           : "";
       return this.label + requiredHtml;
