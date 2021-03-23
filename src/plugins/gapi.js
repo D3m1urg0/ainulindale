2330271cdced7c61a9dc1cc17e5449c956f6bdfd
Pietro Caruso <pietro.caruso@agictech.com>
2021-03-23 13:12:01 +0100

    cookie & google tag manager

---
+++ src/plugins/gapi.js
@@ -0,0 +1,14 @@
+// ***REMOVED***
+
+// ***REMOVED***
+// ***REMOVED***
+// ***REMOVED***
+//       apiKey: "AIzaSyBAiGAXOm1h3M_pyqCN0iWWqfjemJk37F4",
+// ***REMOVED***
+// ***REMOVED***
+// ***REMOVED***
+// ***REMOVED***
+// ***REMOVED***
+// ***REMOVED***
+// ***REMOVED***
+// ***REMOVED***
